68cbdbc2efe5be88a07691ed86790f42c51da4cc
SexyForsakenRebels <39650650+SexyForsakenRebels@users.noreply.github.com>
2018-06-01 19:37:21 -0500

    work on common header, rings update

---
+++ Header.js
@@ -0,0 +1,30 @@
+﻿function writeHeader(linkDepth, fromHome) {
+    if (linkDepth == null) { linkDepth = 0; }
+    var sRoot = "";
+    while (linkDepth > 0) {
+        sRoot += "../";
+        linkDepth--;
+    }
+    document.write("<table>");
+    document.write("<tr>");
+    writeMenuCell("Home", sRoot, null, fromHome);
+    writeMenuCell("Rings", sRoot + "rings", "rings", false);
+    writeMenuCell("Mounts", sRoot + "insigniaBonus", "insigniabonus", false);
+    document.write("</tr>");
+    document.write("</table>");
+}
+
+function writeMenuCell(linkText, linkURL, linkURLBase, fromHome) {
+    var bSelected = (location.href.toLowerCase().indexOf(linkURLBase) != -1) || (linkText == "Home" && (fromHome || false));
+
+    document.write("<th class=\"" + (bSelected ? "selectedMenuCell" : "menuCell") + "\">");
+
+    if (location.href.toLowerCase().indexOf(linkURLBase) == -1) {
+        document.write("<a href=\"" + linkURL + "\">" + linkText + "</a>");
+    }
+    else {
+        document.write(linkText);
+    }
+    document.write("</th>");
+
+}
